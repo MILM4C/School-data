1256addca1005964e2fc7f39c1304ffa3cfe181a
MILM4C <elloses677@gmail.com>
2025-10-01 18:47:30 +0200

    V2
    Belasting leeftijd berekening toegevoegt
    De geschikte onderhoudstaken zijn toegevoegd en de de code pakt het van de database

--- start_code.py
+++ start_code.py
@@ -18,7 +18,7 @@ db = Database(host="localhost", gebruiker="user", wachtwoord="password", databas
 db.connect()
 
 # pas deze query aan om het juiste personeelslid te selecteren
-select_query = "SELECT * FROM personeelslid WHERE id = 1"
+select_query = "SELECT * FROM personeelslid WHERE id = 4"
 personeelslid = db.execute_query(select_query)
 
 # altijd verbinding sluiten met de database als je klaar bent
@@ -44,20 +44,70 @@ db.close()
 
 
 
+
 # verzamel alle benodigde gegevens in een dictionary
+
+# Bereken maximale fysieke belasting op basis van leeftijd
+leeftijd = personeelslid[0]['leeftijd']
+if leeftijd <= 24:
+    max_belasting_leeftijd = 25
+elif leeftijd <= 50:
+    max_belasting_leeftijd = 40
+else:
+    max_belasting_leeftijd = 20
+
+# Gebruik Arboarts waarde als die is ingesteld, anders leeftijdsnorm
+if personeelslid[0]['verlaagde_fysieke_belasting'] > 0:
+    max_fysieke_belasting = personeelslid[0]['verlaagde_fysieke_belasting']
+else:
+    max_fysieke_belasting = max_belasting_leeftijd
+
+# Specialist in attracties omzetten naar array
+specialist_attracties = [attractie.strip() for attractie in personeelslid[0]['specialist_in_attracties'].split(',')] if personeelslid[0]['specialist_in_attracties'] else []
+
 dagtakenlijst = {
-    "personeelsgegevens" : {
-        "naam": personeelslid[0]['naam'] # voorbeeld van hoe je bij een eigenschap komt
-        # STAP 1: vul aan met andere benodigde eigenschappen
+    "personeelsgegevens": {
+        "naam": personeelslid[0]['naam'],
+        "werktijd": personeelslid[0]['werktijd'],
+        "beroepstype": personeelslid[0]['beroepstype'],
+        "bevoegdheid": personeelslid[0]['bevoegdheid'],
+        "specialist_in_attracties": specialist_attracties,
+        "pauze_opsplitsen": bool(personeelslid[0]['pauze_opsplitsen']),
+        "max_fysieke_belasting": max_fysieke_belasting
     },
-    "weergegevens" : {
-        # STAP 4: vul aan met weergegevens
-    }, 
-    "dagtaken": [] # STAP 2: hier komt een lijst met alle dagtaken
-    ,
-    "totale_duur": 0 # STAP 3: aanpassen naar daadwerkelijke totale duur
+    "weergegevens": {
+        "temperatuur": 20,
+        "kans_op_regen": 50
+    },
+    "dagtaken": [],
+    "totale_duur": 0
 }
 
+# STAP 2: Vul de dagtaken lijst
+huidige_duur = 0
+aantal_taken = 0
+
+# Voeg geschikte onderhoudstaken toe
+for taak in onderhoudstaken:
+    if (huidige_duur + taak['duur'] <= personeelslid[0]['werktijd'] and 
+        (taak['fysieke_belasting'] <= max_fysieke_belasting or taak['fysieke_belasting'] == 0)):
+        
+        dagtakenlijst["dagtaken"].append({
+            "omschrijving": taak['omschrijving'],
+            "duur": taak['duur'],
+            "prioriteit": taak['prioriteit'],
+            "beroepstype": taak['beroepstype'],
+            "bevoegdheid": taak['bevoegdheid'],
+            "fysieke_belasting": taak['fysieke_belasting'] if taak['fysieke_belasting'] > 0 else None,
+            "attractie": taak['attractie'],
+            "is_buitenwerk": bool(taak['is_buitenwerk'])
+        })
+        huidige_duur += taak['duur']
+        aantal_taken += 1
+
+# STAP 3: Zet de totale duur
+dagtakenlijst["totale_duur"] = huidige_duur
+
 # uiteindelijk schrijven we de dictionary weg naar een JSON-bestand, die kan worden ingelezen door de acceptatieomgeving
-with open('dagtakenlijst_personeelslid_x.json', 'w') as json_bestand_uitvoer:
+with open('Personeelsgegevens_personeelslid_x.json', 'w') as json_bestand_uitvoer:
     json.dump(dagtakenlijst, json_bestand_uitvoer, indent=4)
